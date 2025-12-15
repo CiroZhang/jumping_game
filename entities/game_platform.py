7d30b969ba4503e62308a284339bdfe7740c40bc
Ciro Zhang <129805733+CiroZhang@users.noreply.github.com>
2025-12-14 23:07:17 -0800

    Delete entities directory

--- entities/game_platform.py
+++
@@ -1,9 +0,0 @@
-from entities.base_entity import BaseEntity
-import pygame
-
-GREEN = (0, 200, 0)
-
-class Platform(BaseEntity):
-    def __init__(self, x, y, width, height=20, color=GREEN, solid=True):
-        super().__init__(x, y, width, height, color)
-        self.solid = solid  # True = can't jump through bottom, False = can
